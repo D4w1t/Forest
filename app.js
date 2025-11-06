26077bb863d16f8a1acec22ff1a0c7f7ca060f6d
Dawit Nigussie <dawit9n@gmail.com>
2025-11-06 15:59:56 +0300

    Implement monogodb connection

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/users.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 
+import connectToDatabase from "./database/mongodb.js";
+
 const app = express();
 
 app.use("/api/v1/auth", authRouter);
@@ -16,10 +18,12 @@ app.get("/", (req, res) => {
   res.send("Welcome to the Subscription Tracker API");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(
     `Subscription Tracker API is running on http://localhost:${PORT}`
   );
+
+  await connectToDatabase();
 });
 
 export default app;

11e200f68136fb489391f8f04825a3d2bfee0487
Dawit Nigussie <dawit9n@gmail.com>
2025-11-07 15:50:24 +0300

    Implement rate limit and bot detection

--- app.js
+++ app.js
@@ -10,13 +10,17 @@ import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
 
 import errorMiddleware from "./middlewares/error.middleware.js";
+import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
 
 const app = express();
 
 //Built in Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use(cookieParser());
+app.use(arcjetMiddleware);
+
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);

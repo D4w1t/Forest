c8b3ef9851a838be3862888cb8604ad570f7a2b0
Dawit Nigussie <dawit9n@gmail.com>
2025-11-07 13:13:42 +0300

    Implement user authentication

--- models/user.model.js
+++ models/user.model.js
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema(
       unique: true,
       lowercase: true,
       trim: true,
-      match: [/\S+@\S+\.S+/, "Please fill a valid email address"],
+      match: [/\S+@\S+\.\S+/, 'Please fill a valid email address'],
     },
     password: {
       type: String,

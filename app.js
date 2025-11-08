765fc082c01332feff9b5adcf49e42f8d4d56ed3
Dawit Nigussie <dawit9n@gmail.com>
2025-11-09 01:03:03 +0300

    Implement reminder workflows

--- app.js
+++ app.js
@@ -11,20 +11,21 @@ import connectToDatabase from "./database/mongodb.js";
 
 import errorMiddleware from "./middlewares/error.middleware.js";
 import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
+import workFlowRouter from "./routes/workflow.routes.js";
 
 const app = express();
 
 //Built in Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use(cookieParser());
-app.use(arcjetMiddleware);
 
+app.use(arcjetMiddleware);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/workflows", workFlowRouter);
 
 app.use(errorMiddleware);
 
